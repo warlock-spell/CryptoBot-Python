7705ddefe8286cb2fad4077084b875719f0e88cc
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-07-06 19:57:13 +0530

    get all active instruments from bitmex

---
+++ bitmex.py
@@ -0,0 +1,13 @@
+# @Project:     Crypto-Bot
+# @Filename:    bitmex.py
+# @Author:      Daksh Gaur
+# @Email:       hi@daksh.fyi
+# @Time:        06-07-2023 07:54 pm
+
+import requests
+
+def get_contracts():
+    response_obj = requests.get("https://www.bitmex.com/api/v1/instrument/active")
+    return [contract['symbol'] for contract in response_obj.json()]
+
+print(get_contracts())
