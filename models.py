6d64f91e8ff0a423a09cc6fe5bac1e5c6128b8ac
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-07-09 14:23:05 +0530

    refactor models

--- models.py
+++ models.py
@@ -6,25 +6,26 @@
 
 
 class Balance:
-    def __init__(self, info):
-        self.initial_margin = float(info['initialMargin'])
-        self.maintenance_margin = float(info['maintMargin'])
-        self.margin_balance = float(info['marginBalance'])
-        self.wallet_balance = float(info['walletBalance'])
-        self.unrealised_pnl = float(info['unrealizedProfit'])
+    def __init__(self, balance_info):
+        self.initial_margin = float(balance_info['initialMargin'])
+        self.maintenance_margin = float(balance_info['maintMargin'])
+        self.margin_balance = float(balance_info['marginBalance'])
+        self.wallet_balance = float(balance_info['walletBalance'])
+        self.unrealised_pnl = float(balance_info['unrealizedProfit'])
 
 
 class Candle:
-    def __init__(self, info):
-        self.open_time = info[0] # already an int
-        self.open = float(info[1])
-        self.high = float(info[2])
-        self.low = float(info[3])
-        self.close = float(info[4])
-        self.volume = float(info[5])
-        self.close_time = info[6]
-        self.quote_asset_volume = float(info[7])
-        self.number_of_trades = float(info[8])
-        self.taker_buy_base_asset_volume = float(info[9])
-        self.taker_buy_quote_asset_volume = float(info[10])
-        self.ignore = float(info[11])
+    def __init__(self, candle_info):
+        self.open_time = candle_info[0] # already an int
+        self.open = float(candle_info[1])
+        self.high = float(candle_info[2])
+        self.low = float(candle_info[3])
+        self.close = float(candle_info[4])
+        self.volume = float(candle_info[5])
+        self.close_time = candle_info[6]
+        self.quote_asset_volume = float(candle_info[7])
+        self.number_of_trades = float(candle_info[8])
+        self.taker_buy_base_asset_volume = float(candle_info[9])
+        self.taker_buy_quote_asset_volume = float(candle_info[10])
+        self.ignore = float(candle_info[11])
+
