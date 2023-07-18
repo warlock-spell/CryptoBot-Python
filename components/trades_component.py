cbe14e55b852b09a17acd657933017e67c6731a7
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-07-18 17:29:20 +0530

    add trades method

--- components/trades_component.py
+++ components/trades_component.py
@@ -5,6 +5,7 @@
 # @Time:        17-07-2023 10:46 pm
 
 import tkinter as tk
+import typing
 
 import components.styles as st
 
@@ -34,3 +35,87 @@ class TradesWatch(tk.Frame):
                 self.body_widgets[f"{val}_var"] = dict()
 
         self._body_index = 1  # Start from 1 because 0 is the header
+
+    def add_trade(self, data: typing.Dict):
+
+        b_index = self._body_index
+
+        t_index = data['time'] # Unique identifier for each trade
+
+        # Time
+
+        self.body_widgets["time"][t_index] = tk.Label(self._table_frame,
+                                                      text=data["time"],
+                                                      bg=st.BG_COLOR_1,
+                                                      fg=st.FG_COLOR_2,
+                                                      font=st.GLOBAL_FONT)
+        self.body_widgets["time"][t_index].grid(row=b_index, column=0)
+
+        # Symbol
+
+        self.body_widgets["symbol"][t_index] = tk.Label(self._table_frame,
+                                                      text=data["symbol"],
+                                                      bg=st.BG_COLOR_1,
+                                                      fg=st.FG_COLOR_2,
+                                                      font=st.GLOBAL_FONT)
+        self.body_widgets["symbol"][t_index].grid(row=b_index, column=1)
+
+        # Exchange
+
+        self.body_widgets["exchange"][t_index] = tk.Label(self._table_frame,
+                                                      text=data["exchange"],
+                                                      bg=st.BG_COLOR_1,
+                                                      fg=st.FG_COLOR_2,
+                                                      font=st.GLOBAL_FONT)
+        self.body_widgets["exchange"][t_index].grid(row=b_index, column=2)
+
+        # Strategy
+
+        self.body_widgets["strategy"][t_index] = tk.Label(self._table_frame,
+                                                          text=data["strategy"],
+                                                          bg=st.BG_COLOR_1,
+                                                          fg=st.FG_COLOR_2,
+                                                          font=st.GLOBAL_FONT)
+        self.body_widgets["strategy"][t_index].grid(row=b_index, column=3)
+
+        # Side
+
+        self.body_widgets["side"][t_index] = tk.Label(self._table_frame,
+                                                          text=data["side"],
+                                                          bg=st.BG_COLOR_1,
+                                                          fg=st.FG_COLOR_2,
+                                                          font=st.GLOBAL_FONT)
+        self.body_widgets["side"][t_index].grid(row=b_index, column=4)
+
+        # Quantity
+
+        self.body_widgets["quantity"][t_index] = tk.Label(self._table_frame,
+                                                            text=data["quantity"],
+                                                            bg=st.BG_COLOR_1,
+                                                            fg=st.FG_COLOR_2,
+                                                            font=st.GLOBAL_FONT)
+        self.body_widgets["quantity"][t_index].grid(row=b_index, column=5)
+
+        # Status
+
+        self.body_widgets["status_var"][t_index] = tk.StringVar()
+        self.body_widgets["status"][t_index] = tk.Label(self._table_frame,
+                                                          textvariable=self.body_widgets["status_var"][t_index],
+                                                          bg=st.BG_COLOR_1,
+                                                          fg=st.FG_COLOR_2,
+                                                          font=st.GLOBAL_FONT)
+        self.body_widgets["status"][t_index].grid(row=b_index, column=6)
+
+        # PnL
+
+        self.body_widgets["pnl_var"][t_index] = tk.StringVar()
+        self.body_widgets["pnl"][t_index] = tk.Label(self._table_frame,
+                                                        textvariable=self.body_widgets["pnl_var"][t_index],
+                                                        bg=st.BG_COLOR_1,
+                                                        fg=st.FG_COLOR_2,
+                                                        font=st.GLOBAL_FONT)
+        self.body_widgets["pnl"][t_index].grid(row=b_index, column=7)
+
+
+        self._body_index += 1
+
