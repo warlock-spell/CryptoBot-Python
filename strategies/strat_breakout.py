5eae88cf91b8fbddc97551ba9763ea88a2d568f1
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-08-22 17:27:13 +0530

    implemented breakout strategy

--- strategies/strat_breakout.py
+++ strategies/strat_breakout.py
@@ -21,4 +21,26 @@ class BreakoutStrategy(Strategy):
 
         self._min_volume = other_params['min_volume']
 
-        print(f"Activated BS for {contract.symbol}")
+        # print(f"Activated BS for {contract.symbol}")
+
+    def _check_signal(self) -> int:
+
+        # at index -1 -> current bar, -2 -> Inside/outside bar depending upon pattern, -3 -> mother bar
+
+        if self.candles[-1].close > self.candles[-2].high and self.candles[-1].volume > self._min_volume:
+            # Long Signal
+            return 1
+        elif self.candles[-1].close < self.candles[-2].low and self.candles[-1].volume > self._min_volume:
+            # Short signal
+            return -1
+        else:
+            # no signal
+            return 0
+
+        # if self.candles[-2].high < self.candles[-3].high and self.candles[-2].low > self.candles[-3].low:
+        #     if self.candles[-1].close > self.candles[-3].high:
+        #         # Upside Breakout
+        #         pass
+        #     elif self.candles[-1].close < self.candles[-3].low:
+        #         # Downside Breakout
+        #         pass
